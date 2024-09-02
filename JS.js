007d9e9b0e6e9545d6edad82af502c4301025c57
ahmed mohamed <ahmedmohamed6x5@gmail.com>
2024-09-03 00:23:01 +0300

    finish home, contact, info pages and adding the background video

--- JS.js
+++ JS.js
@@ -1,10 +1,15 @@
+var BGvideo = document.getElementById("BGvideo");
+
+
 function dark(){  
     document.body.className = "darkmode";
     window.localStorage.setItem("darkmode",1); 
+    BGvideo.src = "bgDarkMode.mp4";
 }
 function white(){
-    document.body.className = "";
+    document.body.className = "body";
     window.localStorage.setItem("darkmode",0);
+    BGvideo.src = "bgWhiteMode.mp4";
 }
 
 
