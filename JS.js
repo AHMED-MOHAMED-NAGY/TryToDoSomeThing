8038f9ca2d6687ef74c94e2876f4c882e571271c
ahmed mohamed <ahmedmohamed6x5@gmail.com>
2024-09-17 23:55:57 +0300

    adding preloader at all of the pages

--- JS.js
+++ JS.js
@@ -5,11 +5,13 @@ function dark(){
     document.body.className = "darkmode";
     window.localStorage.setItem("darkmode",1); 
     BGvideo.src = "bgDarkMode.mp4";
+    loading();
 }
 function white(){
     document.body.className = "body";
     window.localStorage.setItem("darkmode",0);
     BGvideo.src = "bgWhiteMode.mp4";
+    loading();
 }
 
 
@@ -20,4 +22,16 @@ if (DMode == 1){
     dark();
 }else {
     white();
+}
+
+// loader 
+
+window.addEventListener("load",loading())
+
+function loading(){
+    const loader = document.getElementById("loader");
+    loader.classList = "loader";
+    setTimeout(function Loader(){
+        loader.classList ="loader-hidden";
+    },1000)
 }
