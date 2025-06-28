7829cb9a71243b41cd793f41ec926b5247e807c9
ahmed mohamed <ahmedmohamed6x5@gmail.com>
2025-06-28 05:15:53 +0300

    add folders (photos and videos)

--- JS.js
+++ JS.js
@@ -4,13 +4,13 @@ var BGvideo = document.getElementById("BGvideo");
 function dark(){  
     document.body.className = "darkmode";
     window.localStorage.setItem("darkmode",1); 
-    BGvideo.src = "bgDarkMode.mp4";
+    BGvideo.src = "/videos/bgDarkMode.mp4";
     loading();
 }
 function white(){
     document.body.className = "body";
     window.localStorage.setItem("darkmode",0);
-    BGvideo.src = "bgWhiteMode.mp4";
+    BGvideo.src = "/videos/bgWhiteMode.mp4";
     loading();
 }
 
